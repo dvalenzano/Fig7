2eb79ae11d479549b24bcc5644083c94668acf8f
dario valenzano <drvalenzano@gmail.com>
2015-06-25 16:32:01 +0200

    Update LG3_scores.R

--- LG3_scores.R
+++ LG3_scores.R
@@ -2,7 +2,7 @@ library(lattice)
 library(ggplot2)
 library(stats)
 
-gdays <- read.csv('/Volumes/group_dv/personal/DValenzano/Dec2014/surv-sex-logrank.csv', sep=',', header=TRUE)
+gdays <- read.csv('/Volumes/group_dv/personal/DValenzano/month-by-month/Dec2014/surv-sex-logrank.csv', sep=',', header=TRUE)
 # To merge the dot plot for surv-qtl sex and log-rank test values, I need to put everything in the same column
 negl_qvalg <- c(gdays$neg_log.qval., gdays$neg_log.qval._sex, gdays$log.rank) 
 
